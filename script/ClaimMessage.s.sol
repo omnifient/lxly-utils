9b4eddf7ab5d3f24f915d748da9651c5063f61d1
omnifient <90161786+omnifient@users.noreply.github.com>
2024-03-27 11:43:46 +0000

    script for claim message

---
+++ script/ClaimMessage.s.sol
@@ -0,0 +1,382 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {console} from "forge-std/console.sol";
+
+import {IPolygonZkEVMBridgeV2} from "@zkevm/v2/interfaces/IPolygonZkEVMBridgeV2.sol";
+
+// https://etherscan.io/tx/0x6ce3df5f50e3f999926a5c5e6567d8c7110d5d366e8e3d2ed2736eaccc4d3270#eventlog
+//
+// - simulate claimMessage (execute without broadcasting)
+// forge script script/ClaimMessage.s.sol:ClaimMessage --rpc-url https://zkevm-rpc.com -vvvvv
+//
+// - perform the claimMessage (execute with broadcast)
+// forge script script/ClaimMessage.s.sol:ClaimMessage --rpc-url https://zkevm-rpc.com -vvvvv --broadcast
+contract ClaimMessage is Script {
+    function run() public {
+        uint32 originNetwork = 0;
+        uint32 destinationNetwork = 1;
+        address originAddr = 0xbc35bd9A7f1fB02D297E5bf3005f949B8c1A0f91;
+        address destinationAddr = 0xdCDbeb3e5A9E41b3F8EF43E44EEE429A29fDC407;
+
+        vm.startBroadcast(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+
+        IPolygonZkEVMBridgeV2 bridge = IPolygonZkEVMBridgeV2(
+            0x2a3DD3EB832aF982ec71669E178424b10Dca2EDe
+        ); // mainnet bridge
+
+        // values from
+        // https://proof-generator.polygon.technology/api/zkevm/mainnet/bridge?net_id=0&deposit_cnt=176007
+        // https://proof-generator.polygon.technology/api/zkevm/mainnet/merkle-proof?deposit_cnt=176007&net_id=0
+        bridge.claimMessage(
+            // smt proof local exit root
+            [
+                bytes32(
+                    bytes(
+                        hex"1565bf20101808d0b3069ba1695903c861c1540acfd4856b3c6a2fba9e515382"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"3eefadbf1d6b2b8ce757534e9323ce8f0ea24657319a45c5a2175c4d66bf4a14"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"1a2be637e5707ec56541ad6510525f062e1140ef83d951b675dfdfc831b01551"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0216ae314598cf542be3a5f6f893fe8a07914d8ccaa53082e176f03dda2d119c"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"bdc682810bed7d5a7e4ec33131f663e7ffad034ec93648064cf9d993e5af204b"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"954cee305c652930b05e62f755f0cf09f6c565e0ad856c35ab21f0e0124d9468"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"887c22bd8750d34016ac3c66b5ff102dacdd73f6b014e710b51e8022af9a1968"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"654b4d6ff63f39909bfa5b7447be80e13fac2199ea775f9553aa7558c2664812"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"964ee094d88d2fdc2c860f62879c4bb71b12e4e2f67b948c7dc4f4d7e602543b"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"2805776c18ae2c4bf4db1f502240cd53a53fe2e82516a954c6806dcd4de10916"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0f6b459709122e2771f4e4224e12ae21600e9c4ef2e51f7825ff5bc6023aedea"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"34c950316eecce24deeefd4ecf5916b7732aaf8f85f8ec65257150ab2c7c55d3"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"3490c6ceeb450aecdc82e28293031d10c7d73bf85e57bf041a97360aa2c5d99c"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"4fabc9fe01c8f71a973e3647033140a913bc079c663180757558b821d04a10a0"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"5c67add7c6caf302256adedf7ab114da0acfe870d449a3a489f781d659e8becc"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"a5264b93e3b3779e392e44bc1e4edacc2c298eb275a9fc054e78a34576540149"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"2733e50f526ec2fa19a22b31e8ed50f23cd1fdf94c9154ed3a7609a2f1ff981f"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"361122b4b1d18ab577f2aeb6632c690713456a66a5670649ceb2c0a31e43ab46"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"5a2dce0a8a7f68bb74560f8f71837c2c2ebbcbf7fffb42ae1896f13f7c7479a0"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"b46a28b6f55540f89444f63de0378e3d121be09e06cc9ded1c20e65876d36aa0"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"c65e9645644786b620e2dd2ad648ddfcbf4a7e5b1a3a4ecfe7f64667a3f0b7e2"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"f4418588ed35a2458cffeb39b93d26f18d2ab13bdce6aee58e7b99359ec2dfd9"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"5a9c16dc00d6ef18b7933a6f8dc65ccb55667138776f7dea101070dc8796e377"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"4df84f40ae0c8229d0d6069e5c8f39a7c299677a09d367fc7b05e3bc380ee652"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"cdc72595f74c7b1043d0e1ffbab734648c838dfb0527d971b602bc216c9619ef"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0abf5ac974a1ed57f4050aa510dd9c74f508277b39d7973bb2dfccc5eeb0618d"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"b8cd74046ff337f0a7bf2c8e03e10f642c1886798d71806ab1e888d9e5ee87d0"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"838c5655cb21c6cb83313b5a631175dff4963772cce9108188b34ac87c81c41e"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"662ee4dd2dd7b2bc707961b1e646c4047669dcb6584f0d8d770daf5d7e7deb2e"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"388ab20e2573d171a88108e79d820e98f26c0b84aa8b2f4aa4968dbb818ea322"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"93237c50ba75ee485f4c22adf2f741400bdf8d6a9cc7df7ecae576221665d735"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"8448818bb4ae4562849e949e17ac16e0be16688e156b5cf15e098c627c0056a9"
+                    )
+                )
+            ],
+            // smt proof rollup exit root
+            [
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                ),
+                bytes32(
+                    bytes(
+                        hex"0000000000000000000000000000000000000000000000000000000000000000"
+                    )
+                )
+            ],
+            uint256(176007) + 2 ** 64, // global index when originNetwork == 0: uint256(depositCount) + 2 ** 64
+            // uint256(176007) + uint256(1) * 2 ** 32, // global index when originNetwork == 1: uint256(depositCount) + uint256(sourceBridgeNetwork) * _MAX_LEAFS_PER_NETWORK
+            bytes32(
+                bytes(
+                    hex"439e52bd2a7ded13e07355347c5f6f0c388d0e3ce368f30e0d83177c613d829c"
+                )
+            ), // mainnetExitRoot
+            bytes32(
+                bytes(
+                    hex"cc22b6454fe0b5b893cf680099a2a2f704d778be4f167ffa82afc887ce1d67b7"
+                )
+            ), // rollupExitRoot
+            originNetwork,
+            originAddr,
+            destinationNetwork,
+            destinationAddr,
+            0, // amount
+            bytes(
+                hex"00000000000000000000000002023f74ed12df7752144ae8a23411776d4698b400000000000000000000000000000000000000000000000000000000000f4240"
+            ) // metadata
+        );
+    }
+}
